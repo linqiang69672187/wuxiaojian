6af00b3bdf06c0efb987f0f0813da5d1ce5095b7
linqiang69672187 <69672187@qq.com>
2017-11-24 14:52:30 +0800

    首页预警趋势图日期排序错误

--- TaizhouPolice/obj/Release/Package/PackageTmp/comjs/Addcomjs.js
+++ TaizhouPolice/obj/Release/Package/PackageTmp/comjs/Addcomjs.js
@@ -1,9 +1,9 @@
-﻿function currentBasePathGet() {
+﻿function currentBasePathGet() {  
     var strFullPath = window.document.location.href;
     var strPath = window.document.location.pathname;
     var pos = strFullPath.indexOf(strPath);
     var prePath = strFullPath.substring(0, pos + 1);
-    //var postPath = strPath.substring(0, strPath.substr(1).indexOf('/') + 1);
+    //var postPath = strPath.substring(0, strPath.substr(1).indexOf('/') + 1); 
     return prePath; //+"publish/";
 }
 
